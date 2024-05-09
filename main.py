30c11bbca90bb3363427be22551e4f671cc7597d
ksg-dev <ksg.dev.data@gmail.com>
2024-05-09 16:40:46 -0500

    starting steps

---
+++ main.py
@@ -0,0 +1,14 @@
+from game_data import data
+
+
+# Choose random dict entries 1 and 2
+
+# Print twwo options
+
+# Ask user to guess A/B
+
+# If correct - continue 
+
+# clear console except logo - pick 2 is now pick 1, pick a random entry for pick 2
+
+# If wrong, exit, clear console except logo, show score
